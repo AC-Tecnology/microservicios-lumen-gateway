25de21237eb8b0b00659432a3b916538915b78a7
gnuxdar <gnuxdar@gmail.com>
2021-03-11 16:35:23 -0500

    added service in BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,66 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;   //para accerder a los metodos response
-use App\Models\Book;
-//use App\Traits\ApiResponser;    //para acceder a los metodos del trait
+use App\Traits\ApiResponser;    //para acceder a los metodos del trait
 use Illuminate\Http\Request;
+use App\Services\BookService;
+use App\Services\AuthorService;
 
 class BookController extends Controller
 {
-    //use ApiResponser;       //para acceder a los metodos del trait
+    use ApiResponser;       //para acceder a los metodos del trait
+
+    /**
+     * The service to consume the book service
+     * @var BookService
+     */
+    public $bookService;
+
+    /**
+     * The service to consume the author service
+     * @var AuthorService
+     */
+    public $authorService;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
 
-    public function __construct()
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
-        //
+        $this->bookService = $bookService;
     }
 
+
     /**
-     * Retrieve and show all the book
+     * Retrieve and show all the books
      * @return Iluminate\Http\Response
      */
-
     public function index()
     {
+        $responseObtainBooks = $this->bookService->obtainBooks();
+        return $this->successResponse($responseObtainBooks);
+
     }
 
     /**
      * Create a instance of Book
      * @return Iluminate\Http\Response
      */
-
     public function store(Request $request)
     {
+        $responseCreateBooks = $this->bookService->createBook($request->all());
+        return $this->successResponse($responseCreateBooks, Response::HTTP_CREATED);
     }
 
     /**
-     * obtain and show an instance of Book
+     * obtain and show an instance of book
      * @return Iluminate\Http\Response
      */
-
     public function show($id)
     {
+        $responseObtainBook = $this->bookService->obtainBook($id);
+        return $this->successResponse($responseObtainBook);
     }
 
     /**
-     * Updated an instance of Book
+     * Updated an instance of book
      * @return Iluminate\Http\Response
      */
-
     public function update(Request $request, $id)
     {
+        $responseEditBook = $this->bookService->editBook($request->all(),$id);
+        return $this->successResponse($responseEditBook);
     }
 
     /**
-     * Remove an instance of a Book
+     * Remove an instance of a book
      * @return Iluminate\Http\Response
      */
 
     public function destroy($id)
     {
+        $responseDeleteBook = $this->bookService->deleteBook($id);
+        return $this->successResponse($responseDeleteBook);
     }
+
 }
