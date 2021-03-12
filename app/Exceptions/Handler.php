91507ddcb98181765a3034f07adaeaf87b175560
gnuxdar <gnuxdar@gmail.com>
2021-03-12 15:50:15 -0500

    add new exception ClientException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,9 +8,14 @@ use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response;
+use GuzzleHttp\Exception\ClientException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -49,6 +54,91 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        //dd($exception);
+
+        /**
+        *   validar exceptiones HTTP y retornar mensaje en json
+        *   probamos con un metodo y una ruta no valida, ejm DELET con ruta localhost:591/authors
+        */
+        if ($exception instanceof HttpException) {
+
+            $code = $exception->getStatusCode(); //obtener codigo de exception
+            $message = Response::$statusTexts[$code];
+            
+            if ($message == 'Method Not Allowed') {
+                $message = 'Metodo no permitido';
+            }
+
+            return $this->errorResponse($message, $code);
+
+        }
+
+        /**
+         *  validar exceptiones Models y retornar mensaje en json
+         *  probamos con un eliminar una instancia que no existe, ejm DELET con ruta localhost:591/authors/234234
+         */
+        if ($exception instanceof ModelNotFoundException) {
+
+            $code = Response::HTTP_NOT_FOUND;
+            $model = strtolower(class_basename($exception->getModel()));
+
+            return $this->errorResponse("No se encuentra esta instancia {$model} con el id especificado", $code);
+
+        }
+
+        /**
+         *
+         */
+        if ($exception instanceof AuthorizationException) {
+
+            $code = Response::HTTP_FORBIDDEN;
+
+            return $this->errorResponse($exception->getMessage(), $code);
+
+        }
+
+        /**
+         * 
+         */
+        if ($exception instanceof AuthenticationException) {
+
+            $code = Response::HTTP_UNAUTHORIZED;
+
+            return $this->errorResponse($exception->getMessage(), $code);
+
+        }
+
+        /**
+         * Validar las validaciones al actualizar y retornar mensaje en json
+         * guardar un author sin nada, POST localhost:591/authors/
+         */
+        if ($exception instanceof ValidationException) {
+
+            $errors = $exception->validator->errors()->getMessages();
+            $code = Response::HTTP_UNPROCESSABLE_ENTITY;
+
+            return $this->errorResponse($errors, $code);
+
+        }
+
+        /**
+         * validar al cliente
+         */
+        if ($exception instanceof ClientException) {
+            $message = json_decode($exception->getResponse()->getBody()->getContents());
+            $code = $exception->getCode();
+
+            return $this->errorMessage($message, $code);
+        }
+
+        /**
+         * 
+         */
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected Error. Try Later', Response::HTTP_INTERNAL_SERVER_ERROR);
+
     }
 }
