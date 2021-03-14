de67462496b3ac0db9c531a3fa3085de8c5e92cc
gnuxdar <gnuxdar@gmail.com>
2021-03-13 19:08:56 -0500

    updated with new group routes

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,36 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-/**
- * Authors routes
- */
-$router->get('/authors', 'AuthorController@index');
-$router->post('/authors', 'AuthorController@store');
-$router->get('/authors/{id}', 'AuthorController@show');
-$router->put('/authors/{id}', 'AuthorController@update');
-$router->patch('/authors/{id}', 'AuthorController@update');
-$router->delete('/authors/{id}', 'AuthorController@destroy');
 
-/**
- * Book routes
- */
-$router->get('/books', 'BookController@index');
-$router->post('/books', 'BookController@store');
-$router->get('/books/{id}', 'BookController@show');
-$router->put('/books/{id}', 'BookController@update');
-$router->patch('/books/{id}', 'BookController@update');
-$router->delete('/books/{id}', 'BookController@destroy');
+$router->group(['middleware' => 'client.credentials'], function() use ($router){
+
+	/**
+	 * Authors routes
+	 */
+	$router->get('/authors', 'AuthorController@index');
+	$router->post('/authors', 'AuthorController@store');
+	$router->get('/authors/{id}', 'AuthorController@show');
+	$router->put('/authors/{id}', 'AuthorController@update');
+	$router->patch('/authors/{id}', 'AuthorController@update');
+	$router->delete('/authors/{id}', 'AuthorController@destroy');
+
+	/**
+	 * Book routes
+	 */
+	$router->get('/books', 'BookController@index');
+	$router->post('/books', 'BookController@store');
+	$router->get('/books/{id}', 'BookController@show');
+	$router->put('/books/{id}', 'BookController@update');
+	$router->patch('/books/{id}', 'BookController@update');
+	$router->delete('/books/{id}', 'BookController@destroy');
+
+	/**
+	 * User routes
+	 */
+	$router->get('/users', 'UserController@index');
+	$router->post('/users', 'UserController@store');
+	$router->get('/users/{id}', 'UserController@show');
+	$router->put('/users/{id}', 'UserController@update');
+	$router->patch('/users/{id}', 'UserController@update');
+	$router->delete('/users/{id}', 'UserController@destroy');
+});
