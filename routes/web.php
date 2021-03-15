aada17c81c898eb6688964ddaaf05ccf7e3edf60
gnuxdar <gnuxdar@gmail.com>
2021-03-14 20:59:29 -0500

    add new group routes to client

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,10 @@ $router->group(['middleware' => 'client.credentials'], function() use ($router){
 	$router->patch('/users/{id}', 'UserController@update');
 	$router->delete('/users/{id}', 'UserController@destroy');
 });
+
+/**
+ * Routes protected by user credentials
+ */
+$router->group(['middleware' => 'auth:api'], function() use ($router){
+	$router->get('/users/me', 'UserController@me');
+});
